b14eb96aa1658174ee470a255d0d9863a2150890
Attoh, Stefan <SXA3808@yum.com>
2024-04-08 10:27:15 -0600

    Update FormTextField Component

--- src/components/FormTextField/FormTextField.test.tsx
+++ src/components/FormTextField/FormTextField.test.tsx
@@ -1,12 +1,18 @@
-import { render, screen } from "@testing-library/react-native"
+import { fireEvent, render, screen } from "@testing-library/react-native"
 import FormTextField from "./FormTextField"
 
 describe("FormTextField component", () => {
   it("renders label", () => {
     const handleChangeMock = jest.fn()
-    render(<FormTextField label="Hello!" value="response" onChange={handleChangeMock}></FormTextField>)
+    render(
+      <FormTextField
+        label="Hello!"
+        value="response"
+        onChange={handleChangeMock}
+      ></FormTextField>,
+    )
     expect(screen.getByText(/Hello/)).toBeOnTheScreen()
-    user.keyboard(screen.getByLabelText(/Hello/i), "test")
+    fireEvent.changeText(screen.getByLabelText(/Hello/i), "test")
     expect(handleChangeMock).toHaveBeenCalledWith("test")
   })
 })
