031015bac96c2e529c31206b40294c1b3a913cc6
Stefan Attoh <73801555+snoattoh@users.noreply.github.com>
2024-04-19 11:25:21 -0600

    (LD-473) Add data migration (#46)

--- src/services/storage/storage.ts
+++ src/services/storage/storage.ts
@@ -6,7 +6,15 @@ export const getData = async <T>(key: string): Promise<T | undefined> => {
   return jsonValue
 }
 
+export const getAllKeys = (): Promise<readonly string[]> => {
+  return AsyncStorage.getAllKeys()
+}
+
 export const storeData = <T>(key: string, value: T): Promise<void> => {
   const jsonValue = JSON.stringify(value)
   return AsyncStorage.setItem(key, jsonValue)
+}
+
+export const clearStorage = (): Promise<void> => {
+  return AsyncStorage.clear()
 }
